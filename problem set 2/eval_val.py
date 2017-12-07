24bc6b95f3b8bdf3a7974f359b48d900e269d071
Divya0406 <33832718+Divya0406@users.noreply.github.com>
2017-12-07 10:33:20 +0530

    added

---
+++ problem set 2/eval_val.py
@@ -0,0 +1,40 @@
+'''Doc: solution <problemset 2>
+
+Question : 4
+
+Author: divya.natarajan@accenture.com
+
+Date:05/12/2017'''
+
+
+
+import math
+
+def eval_val():
+
+  
+    while True:
+
+    
+
+    
+        str = raw_input('Enter the string : ')
+
+    
+        if str == 'done' :
+
+      
+            break
+
+    
+        else:
+
+      
+            res=eval(str)
+
+      
+            print res
+
+
+
+eval_val()
